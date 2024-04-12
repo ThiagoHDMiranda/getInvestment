25a75842bcc4c56428afeef2c8c81ca591b28602
ThiagoHDMiranda <106932990+ThiagoHDMiranda@users.noreply.github.com>
2024-04-12 12:37:04 -0300

    remove comment lines

--- pages/statusInvest.js
+++ pages/statusInvest.js
@@ -2,16 +2,14 @@ import puppeteer from "puppeteer";
 
 async function getAsset(asset) {
   const browser = await puppeteer.launch({ headless: true });
-
   const page = await browser.newPage();
-
   const url = `https://statusinvest.com.br/acoes/${asset}`;
 
   await page.setUserAgent(
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
   );
   await page.goto(url);
-  page.setDefaultTimeout(2000);
+
   const priceXpath =
     '//*[@id="main-2"]/div[2]/div/div[1]/div/div[1]/div/div[1]/strong';
 
@@ -32,8 +30,5 @@ async function getAsset(asset) {
 
   return result;
 }
-
-// const asset = await getAsset("petr4");
-// console.log(asset);
 
 export { getAsset };
